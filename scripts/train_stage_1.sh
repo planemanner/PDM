0fe488daf468456143a0b35d787aa5ab6dbf4022
unknown <smddls77@gmail.com>
2025-03-06 21:57:40 +0900

    upload stage 1 script

--- scripts/train_stage_1.sh
+++ scripts/train_stage_1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-# This stage trains VQ-VAE
+save_dir=/my_path
+seed=42
+
+python ../main.py --stage autoencoder \
+                  --save_dir $save_dir
+                  --seed $seed
